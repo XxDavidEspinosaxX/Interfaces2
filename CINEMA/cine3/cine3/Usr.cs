7b0a18e4aca57d7138e484303792929595169d89
lorena <lorena.94mc@gmail.com>
2024-11-28 17:37:35 +0100

    añado class usr

---
+++ CINEMA/cine3/cine3/Usr.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cine3
+{
+    public class Usr
+    { 
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public Usr(string email_intro, string passw_intro)
+        {
+            this.Email = email_intro; this.Password = passw_intro; 
+        }
+        public Usr() 
+        {
+            this.Email = "";
+            this.Password = "";
+        }
+    }
+}
